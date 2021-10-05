a8fa1cf89387f0cad218a0188fc702c4286ab7f7
Milena Diniz Vieira <miladvieira@outlook.com>
2021-10-05 13:50:51 -0300

    Add project files.

---
+++ Calculadora/Operadores.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace Calculadora {
+    class Operadores {
+        public static decimal Calcular (int num1, int num2, string operador) {
+
+            decimal resultado;
+
+            switch (operador) 
+            {
+                case "+":
+                    resultado = Somar(num1, num2);
+                    break;
+                case "-":
+                    resultado = Subtrair(num1, num2);
+                    break;
+                case "*":
+                    resultado = Multiplicar(num1, num2);
+                    break;
+                case "/":
+                    resultado = Dividir(num1, num2);
+                    break;
+                default:
+                    resultado = 0;
+                    break;
+            }
+            return resultado;
+        }
+
+        private static decimal Somar(decimal num1, decimal num2) { // private para ninguém chama-ló fora da classe e static para não precisar fazer uma instancia.
+
+            return num1 + num2;
+        }
+
+        private static decimal Subtrair(decimal num1, decimal num2) {
+            return num1 - num2;
+        }
+
+        private static decimal Multiplicar(decimal num1, decimal num2) {
+            return num1 * num2;
+        }
+        private static decimal Dividir(decimal num1, decimal num2) {
+            return num1 / num2;
+        }
+
+
+
+
+
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculadora {
+    class Program {
+        static void Main(string[] args) {
+
+            int num1, num2;
+            decimal resultado;
+
+            string operador;
+
+            Console.Write("Primeiro número: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Operador:  ");
+            operador = Console.ReadLine();
+
+            Console.Write("Segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            resultado = Operadores.Calcular(num1, num2, operador);
+
+
+            Console.WriteLine($"Calculo: {num1}{operador}{num2}={resultado}");
+
+
+        }
+    }
+}
